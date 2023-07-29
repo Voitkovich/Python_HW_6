0e6a6312b8e5afb492106da8133211b990ba912e
Ekaterina Voitkovich <styardess@mail.ru>
2023-07-29 13:08:10 +0300

    finish

--- HW_2.py
+++ HW_2.py
@@ -1,46 +1,46 @@
 # Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
 # Функцию hex используйте для проверки своего результата.
 
-# num = int(input("Введите число: "))
-# print(hex(num))
-# sixt = ''
-# hexx = '0123456789ABCDEF'
-# SIX = 16
+num = int(input("Введите число: "))
+print(hex(num))
+sixt = ''
+hexx = '0123456789ABCDEF'
+SIX = 16
 
-# while num > 0:
-#     sixt = hexx[num % SIX] + sixt
-#     num = num // SIX
+while num > 0:
+    sixt = hexx[num % SIX] + sixt
+    num = num // SIX
 
-# print(sixt)
+print(sixt)
 
 # Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
 # Программа должна возвращать сумму и произведение* дробей. 
 # Для проверки своего кода используйте модуль fractions.
 
 
-# def process(num, div):
+def process(num, div):
    
-#     numer1, denom1 = map(int, num.split("/"))
-#     numer2, denom2 = map(int, div.split("/"))
+    numer1, denom1 = map(int, num.split("/"))
+    numer2, denom2 = map(int, div.split("/"))
 
-#     sum_num = numer1 * denom2 + numer2 * denom1
-#     sum_denom = denom1 * denom2
-#     sum_frac = (sum_num, sum_denom)
+    sum_num = numer1 * denom2 + numer2 * denom1
+    sum_denom = denom1 * denom2
+    sum_frac = (sum_num, sum_denom)
 
-#     prod_num = numer1 * numer2
-#     prod_denom = denom1 * denom2
-#     prod_frac = (prod_num, prod_denom)
+    prod_num = numer1 * numer2
+    prod_denom = denom1 * denom2
+    prod_frac = (prod_num, prod_denom)
 
-#     return sum_frac, prod_frac
+    return sum_frac, prod_frac
 
 
-# num = input("Введите первую дробь: ")
-# div = input("Введите вторую дробь: ")
+num = input("Введите первую дробь: ")
+div = input("Введите вторую дробь: ")
 
-# sum_frac, prod_frac = process(num, div)
+sum_frac, prod_frac = process(num, div)
 
-# print(f"Сумма дробей {num} и {div} - {sum_frac[0]}/{sum_frac[1]}")
-# print(f"Произведение дробей {num} и {div} - {prod_frac[0]}/{prod_frac[1]}")
+print(f"Сумма дробей {num} и {div} - {sum_frac[0]}/{sum_frac[1]}")
+print(f"Произведение дробей {num} и {div} - {prod_frac[0]}/{prod_frac[1]}")
 
 
 # Задание №6
